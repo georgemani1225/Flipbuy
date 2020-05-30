4d1a14ff6728cbfc444bae865a04821ddc3c4350
Shalini <shalinimanayath149@gmail.com>
2020-05-30 22:56:44 +0530

    Merge branch 'master' of C:\Users\Shalini\AndroidStudioProjects\Flipbuy with conflicts.

---
+++ app/src/main/java/com/cmi/flipbuy/Checkout.kt
@@ -0,0 +1,12 @@
+package com.cmi.flipbuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Checkout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
--- app/src/main/java/com/cmi/flipbuy/fragment/CartFragment.kt
+++ app/src/main/java/com/cmi/flipbuy/fragment/CartFragment.kt
@@ -5,19 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cmi.flipbuy.R
+import kotlinx.android.synthetic.main.fragment_my_cart.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class CartFragment : Fragment() {
+    lateinit var recyclerview:RecyclerView
+    lateinit var layoutManager:RecyclerView.LayoutManager
+    lateinit var btnNext:Button
+    lateinit var txtTotalPrice:TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_cart, container, false)
+
+
     }
+
 
 }
