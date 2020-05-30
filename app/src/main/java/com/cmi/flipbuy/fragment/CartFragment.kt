8edbdc8c5b084a28cbfb6844f22b86865aa6ee53
Shalini <shalinimanayath149@gmail.com>
2020-05-30 22:57:26 +0530

    Cart unfinished

--- app/src/main/java/com/cmi/flipbuy/fragment/CartFragment.kt
+++ app/src/main/java/com/cmi/flipbuy/fragment/CartFragment.kt
@@ -1,6 +1,8 @@
 package com.cmi.flipbuy.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,26 +11,40 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.cmi.flipbuy.Checkout
 import com.cmi.flipbuy.R
+import com.cmi.flipbuy.activity.AdminAddProductActivity
 import kotlinx.android.synthetic.main.fragment_my_cart.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class CartFragment : Fragment() {
     lateinit var recyclerview:RecyclerView
     lateinit var layoutManager:RecyclerView.LayoutManager
     lateinit var btnNext:Button
     lateinit var txtTotalPrice:TextView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_my_cart, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_my_cart, container, false)
+
+        btnNext.setOnClickListener{
+            Log.d("btnSetup", "Selected")
+        }
+        recyclerview=view.findViewById(R.id.CartList)
+        layoutManager=LinearLayoutManager(activity)
+        return view
+
+
 
 
     }
+    companion object {
+        fun onCreate(): CartFragment {
+            return CartFragment()
+        }
+    }
+
 
 
 }
