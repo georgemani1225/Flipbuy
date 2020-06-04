299c4ea2ecc175d3ec1cfd84b6483a6cf92ee891
george mani <georgemani1225@gmail.com>
2020-06-05 00:54:43 +0530

    edited dashboard fragment

--- app/src/main/java/com/cmi/flipbuy/fragment/DashboardFragment.kt
+++ app/src/main/java/com/cmi/flipbuy/fragment/DashboardFragment.kt
@@ -1,36 +1,22 @@
 package com.cmi.flipbuy.fragment
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.GridView
 
-import androidx.recyclerview.widget.RecyclerView
 import com.cmi.flipbuy.R
-import com.cmi.flipbuy.adapter.DashboardRecyclerAdapter
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.recycler_dashboard_single_row.*
+import com.cmi.flipbuy.adapter.ProductsAdapter
+import com.cmi.flipbuy.model.Product
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class DashboardFragment : Fragment() {
-    lateinit var recyclerDashboard: RecyclerView
-    lateinit var LayoutManager: RecyclerView.LayoutManager
-    var products=ArrayList<String>()
 
-   /*val productList = arrayListOf<products>(
-        Product("Shirts", "Blue", "999/-", "4.5", "", R.id.imgProductImage),
-        Product("Frocks", "Red", "500/-", "4", "", R.id.imgProductImage)
-    )*/
-    lateinit var recyclerAdapter: DashboardRecyclerAdapter
-
+    private var arrayList: ArrayList<Product> ? = null
+    private var gridView: GridView? = null
+    private var productsAdapter: ProductsAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,46 +24,7 @@ class DashboardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        recyclerDashboard = view.findViewById(R.id.recyclerDashboard)
-        LayoutManager = LinearLayoutManager(activity)
-        recyclerAdapter = DashboardRecyclerAdapter(activity as Context,products)
-        recyclerDashboard.adapter = recyclerAdapter
-        recyclerDashboard.layoutManager = LayoutManager
 
-
-        val ref: DatabaseReference = FirebaseDatabase.getInstance().getReference("ProductInfo")
-            .child(FirebaseAuth.getInstance().currentUser!!.uid)
-
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.forEach {
-                    //val name:DatabaseReference=ref.child("name")
-
-                    val name:String? = dataSnapshot.child("name").getValue().toString()
-                    txtNameOfProduct.text = name
-                    val PdtName:String=txtNameOfProduct.toString()
-                    val descrip:String?=dataSnapshot.child("descrip").getValue().toString()
-                    txtProduct_Description.text=descrip
-                    val PdtDescrip:String=txtProduct_Description.toString()
-                    val price:String?=dataSnapshot.child("price").getValue().toString()
-                    txtPrice.text=price
-                    val PdtPrice:String=txtPrice.toString()
-                    val productImage:String?=dataSnapshot.child("productImage").getValue().toString()
-                    Picasso.get().load(productImage).error(R.drawable.polo).into(imgProductImage)
-                    //val PdtImage:String
-                    products= arrayListOf<String>(
-                        PdtName,PdtDescrip,PdtPrice//,productImage
-                    )
-
-                    Log.d("DashboardFragment", it.toString())
-
-                }
-            }
-        })
 
         return view
 
--- app/src/main/java/com/cmi/flipbuy/model/Product.kt
+++ app/src/main/java/com/cmi/flipbuy/model/Product.kt
@@ -1,17 +1,16 @@
 package com.cmi.flipbuy.model
 
-import com.cmi.flipbuy.fragment.DashboardFragment
-
-data class Product(
-    var ProductName: String,
-    var ProductDescrip: String,
-    var productPrice: String,
-    var productRating: String,
-    var uid: String,
-    var productImage: Int
-) {
-
-    constructor() : this("", "", "", "", "", 0)
-
-
+class Product{
+    var productName: String? = null
+    var productPrice: String? = null
+    var productImage: Int? = 0
+    constructor(ProductName: String?, ProductPrice: String?, ProductImage: Int?){
+        this.productName = ProductName
+        this.productImage = ProductImage
+        this.productPrice = ProductPrice
+    }
 }
+
+
+
+
