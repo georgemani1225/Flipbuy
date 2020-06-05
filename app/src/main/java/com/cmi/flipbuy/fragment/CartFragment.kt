170bc5ea5222a3b7f41369928fe94e0a11036c9e
Shalini <shalinimanayath149@gmail.com>
2020-06-05 23:08:28 +0530

    Cart

--- app/src/main/java/com/cmi/flipbuy/fragment/CartFragment.kt
+++ app/src/main/java/com/cmi/flipbuy/fragment/CartFragment.kt
@@ -18,10 +18,6 @@ import kotlinx.android.synthetic.main.fragment_my_cart.*
 import kotlinx.android.synthetic.main.fragment_my_cart.view.*
 
 class CartFragment : Fragment() {
-    lateinit var recyclerview:RecyclerView
-    lateinit var layoutManager:RecyclerView.LayoutManager
-    lateinit var btnNext:Button
-    lateinit var txtTotalPrice:TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -29,22 +25,15 @@ class CartFragment : Fragment() {
 
         val view: View = inflater!!.inflate(R.layout.fragment_my_cart, container, false)
 
-        view.btnNext.setOnClickListener{
 
-        }
-        recyclerview=view.findViewById(R.id.CartList)
-        layoutManager=LinearLayoutManager(activity)
+
         return view
 
 
 
 
     }
-    companion object {
-        fun onCreate(): CartFragment {
-            return CartFragment()
-        }
-    }
+
 
 
 
