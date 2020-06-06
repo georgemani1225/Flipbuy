9aff3d726e86ebeaab687b5c333e831aa26a456d
george mani <georgemani1225@gmail.com>
2020-06-07 01:38:21 +0530

    added product details activity using firebase

--- app/src/main/java/com/cmi/flipbuy/activity/ProductDetailsActivity.kt
+++ app/src/main/java/com/cmi/flipbuy/activity/ProductDetailsActivity.kt
@@ -1,14 +1,12 @@
 package com.cmi.flipbuy.activity
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
-import androidx.fragment.app.Fragment
 import com.cmi.flipbuy.R
-import com.cmi.flipbuy.fragment.DashboardFragment
 import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_product_details.*
@@ -16,7 +14,12 @@ import kotlinx.android.synthetic.main.activity_product_details.*
 
 class ProductDetailsActivity : AppCompatActivity() {
 
-    lateinit var cardShirt1: CardView
+    lateinit var btnCart: Button
+    lateinit var pdt_Image: ImageView
+    lateinit var txtPdt_Name: TextView
+    lateinit var txt_PdtDesc: TextView
+    lateinit var txt_PdtPrice: TextView
+    private var pdtID: String? = null
     lateinit var mDatabase: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,27 +27,28 @@ class ProductDetailsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        cardShirt1 = findViewById(R.id.cardShirt1)
-        cardShirt1.setOnClickListener {
-            mDatabase = FirebaseDatabase.getInstance().getReference("Products")
-                .child("shirt_1")
-            mDatabase.addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {
+        btnCart = findViewById(R.id.btnCart)
+        pdt_Image = findViewById(R.id.pdt_Image)
+        txtPdt_Name = findViewById(R.id.txtPdt_Name)
+        txt_PdtDesc = findViewById(R.id.txtPdt_Desc)
+        txt_PdtPrice = findViewById(R.id.txtPdt_Price)
+        pdtID = intent.getStringExtra("pid")
+        mDatabase = FirebaseDatabase.getInstance().getReference("Products").child(pdtID!!)
+        mDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
 
-                }
+            }
 
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val Nresult = dataSnapshot.child("ProductName").getValue().toString()
-                    val Iresult = dataSnapshot.child("ProductImg").getValue().toString()
-                    val DResult = dataSnapshot.child("ProductDesc").getValue().toString()
-                    val Presult = dataSnapshot.child("ProductPrice").getValue().toString()
-                    txtPdt_Name.text = Nresult
-                    txtPdt_Desc.text = DResult
-                    txtPdt_Price.text = Presult
-                    Picasso.get().load(Iresult).into(pdt_Image)
-
-                }
-            })
-        }
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val Nresult = dataSnapshot.child("ProductName").getValue().toString()
+                val Iresult = dataSnapshot.child("ProductImg").getValue().toString()
+                val Dresult = dataSnapshot.child("ProductDesc").getValue().toString()
+                val Presult = dataSnapshot.child("ProductPrice").getValue().toString()
+                txtPdt_Name.text = Nresult
+                txt_PdtDesc.text = Dresult
+                txt_PdtPrice.text = Presult
+                Picasso.get().load(Iresult).into(pdt_Image)
+            }
+        })
     }
 }
--- app/src/main/java/com/cmi/flipbuy/fragment/DashboardFragment.kt
+++ app/src/main/java/com/cmi/flipbuy/fragment/DashboardFragment.kt
@@ -14,11 +14,24 @@ import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_drawer_header.*
 import kotlinx.android.synthetic.main.activity_product_details.*
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 
 class DashboardFragment : Fragment() {
 
-    lateinit var cardShirt1: CardView
+    lateinit var cShirt1: CardView
+    lateinit var cShirt2: CardView
+    lateinit var cShirt3: CardView
+    lateinit var cShirt4: CardView
+    lateinit var cShirt5: CardView
+    lateinit var cShirt6: CardView
+    lateinit var cFrock1: CardView
+    lateinit var cFrock2: CardView
+    lateinit var cFrock3: CardView
+    lateinit var cFrock4: CardView
+    lateinit var cFrock5: CardView
+    lateinit var cFrock6: CardView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,12 +39,79 @@ class DashboardFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
-        cardShirt1 = view.findViewById(R.id.cardShirt1)
-        cardShirt1.setOnClickListener {
+        cShirt1 = view.findViewById(R.id.cShirt1)
+        cShirt2 = view.findViewById(R.id.cShirt2)
+        cShirt3 = view.findViewById(R.id.cShirt3)
+        cShirt4 = view.findViewById(R.id.cShirt4)
+        cShirt5 = view.findViewById(R.id.cShirt5)
+        cShirt6 = view.findViewById(R.id.cShirt6)
+        cFrock1 = view.findViewById(R.id.cFrock1)
+        cFrock2 = view.findViewById(R.id.cFrock2)
+        cFrock3 = view.findViewById(R.id.cFrock3)
+        cFrock4 = view.findViewById(R.id.cFrock4)
+        cFrock5 = view.findViewById(R.id.cFrock5)
+        cFrock6 = view.findViewById(R.id.cFrock6)
+
+        cShirt1.setOnClickListener {
             val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "shirt_1")
             startActivity(intent)
         }
-
+        cShirt2.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "shirt_2")
+            startActivity(intent)
+        }
+        cShirt3.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "shirt_3")
+            startActivity(intent)
+        }
+        cShirt4.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "shirt_4")
+            startActivity(intent)
+        }
+        cShirt5.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "shirt_5")
+            startActivity(intent)
+        }
+        cShirt6.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "shirt_6")
+            startActivity(intent)
+        }
+        cFrock1.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "frock_1")
+            startActivity(intent)
+        }
+        cFrock2.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "frock_2")
+            startActivity(intent)
+        }
+        cFrock3.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "frock_3")
+            startActivity(intent)
+        }
+        cFrock4.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "frock_4")
+            startActivity(intent)
+        }
+        cFrock5.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "frock_5")
+            startActivity(intent)
+        }
+        cFrock6.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            intent.putExtra("pid", "frock_6")
+            startActivity(intent)
+        }
         return view
     }
 }
