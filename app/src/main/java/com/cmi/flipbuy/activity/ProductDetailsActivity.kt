d8596ba16fe944850b11928ec8be8b580b07428f
george mani <georgemani1225@gmail.com>
2020-06-06 21:25:21 +0530

    details activity

--- app/src/main/java/com/cmi/flipbuy/activity/ProductDetailsActivity.kt
+++ app/src/main/java/com/cmi/flipbuy/activity/ProductDetailsActivity.kt
@@ -1,37 +1,50 @@
 package com.cmi.flipbuy.activity
 
-import android.media.Image
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.cmi.flipbuy.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.cmi.flipbuy.fragment.DashboardFragment
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_product_details.*
 
 
 class ProductDetailsActivity : AppCompatActivity() {
-    lateinit var imageDetails:ImageView
-    lateinit var txtProduct_name:TextView
-    lateinit var txtProduct_Descrip:TextView
-    lateinit var txtProduct_price:TextView
-    lateinit var btnItemsNum:ElegantNumberButton
-    lateinit var addToCart:FloatingActionButton
 
-
+    lateinit var cardShirt1: CardView
+    lateinit var mDatabase: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
-        txtProduct_name=findViewById(R.id.txtPdt_details)
-        txtProduct_Descrip=findViewById(R.id.txtPdt_descrip)
-        txtProduct_price=findViewById(R.id.txtPdt_price)
-        btnItemsNum=findViewById(R.id.num_btn)
-        imageDetails=findViewById(R.id.pdt_image_details)
-        addToCart=findViewById(R.id.add_product_to_cart)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        cardShirt1 = findViewById(R.id.cardShirt1)
+        cardShirt1.setOnClickListener {
+            mDatabase = FirebaseDatabase.getInstance().getReference("Products")
+                .child("shirt_1")
+            mDatabase.addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
 
+                }
 
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val Nresult = dataSnapshot.child("ProductName").getValue().toString()
+                    val Iresult = dataSnapshot.child("ProductImg").getValue().toString()
+                    val DResult = dataSnapshot.child("ProductDesc").getValue().toString()
+                    val Presult = dataSnapshot.child("ProductPrice").getValue().toString()
+                    txtPdt_Name.text = Nresult
+                    txtPdt_Desc.text = DResult
+                    txtPdt_Price.text = Presult
+                    Picasso.get().load(Iresult).into(pdt_Image)
 
-
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/cmi/flipbuy/fragment/DashboardFragment.kt
+++ app/src/main/java/com/cmi/flipbuy/fragment/DashboardFragment.kt
@@ -1,19 +1,24 @@
 package com.cmi.flipbuy.fragment
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
-
+import androidx.cardview.widget.CardView
 import com.cmi.flipbuy.R
+import com.cmi.flipbuy.activity.ProductDetailsActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_drawer_header.*
+import kotlinx.android.synthetic.main.activity_product_details.*
 
 
 class DashboardFragment : Fragment() {
 
-
+    lateinit var cardShirt1: CardView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,9 +26,13 @@ class DashboardFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
+        cardShirt1 = view.findViewById(R.id.cardShirt1)
+        cardShirt1.setOnClickListener {
+            val intent = Intent(activity, ProductDetailsActivity::class.java)
+            startActivity(intent)
+        }
+
         return view
-
-
     }
 }
 
