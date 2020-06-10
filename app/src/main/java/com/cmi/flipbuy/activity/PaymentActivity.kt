2893a6a39f76c3efef9ef7071a9d391b1e84cb08
george mani <georgemani1225@gmail.com>
2020-06-10 12:21:20 +0530

    added cart and payment activity

--- app/src/main/java/com/cmi/flipbuy/activity/PaymentActivity.kt
+++ app/src/main/java/com/cmi/flipbuy/activity/PaymentActivity.kt
@@ -1,13 +1,20 @@
 package com.cmi.flipbuy.activity
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.cmi.flipbuy.R
 
-class Checkout : AppCompatActivity() {
+class PaymentActivity : AppCompatActivity() {
+
+    lateinit var btnCheckout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_checkout)
+        setContentView(R.layout.activity_payment)
+
+
     }
 }
